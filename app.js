8e359621649f5d1a3f00f300b9ab753052b85cbb
Seth Koch <sethkoch1@gmail.com>
2016-10-17 16:52:33 -0500

    One tiny little chage

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 
 // app.set('port', (process.env.PORT || 5000));
 app.listen(process.env.PORT || 5000);
-app.use(express.static('/client/index.html'));
+app.use(express.static('client/index.html'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
